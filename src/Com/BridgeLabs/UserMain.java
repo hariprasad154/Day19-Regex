da4539659d5f3fb053b570b0ec7ea957cce9393e
Hari Prasad G <hariprasas154@gmail.com>
2023-02-05 00:03:24 +0530

    the firstName validation method done

--- src/Com/BridgeLabs/UserMain.java
+++ src/Com/BridgeLabs/UserMain.java
@@ -1,19 +1,11 @@
 package Com.BridgeLabs;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class UserMain {
-	static Pattern pattern = Pattern.compile("^[a-z0-9]?[a-z]{2,}$");
+	
 
     public static void main(String[] args) {
-        
-        Matcher matcher = pattern.matcher("bcc");
-        if (matcher.matches()) {
-            System.out.println("Valid");
-        } else {
-            System.out.println("Invalid");
-        }
-
+    	UserRegistation obj1=new UserRegistation();
+    	obj1.UserFirstName("Hari");
     }
 }
--- src/Com/BridgeLabs/UserRegistation.java
+++ src/Com/BridgeLabs/UserRegistation.java
@@ -1,5 +1,18 @@
 package Com.BridgeLabs;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistation {
+	public void UserFirstName(String Firstname) {
+		Pattern FirstNamepattern = Pattern.compile("^[A-Z]+[a-z]{2,}$");
+		Matcher matcher = FirstNamepattern.matcher(Firstname);
+        if (matcher.matches()) {
+            System.out.println(Firstname +" -Valid");
+        } else {
+            System.out.println(Firstname +" is Invalid");
+        }
+	}
+
 
 }
